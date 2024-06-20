cab87b3327f6c05aae6455ee4ffee571d947a463
Ajinkya <ajinkyatambade143@gmail.com>
2024-06-20 10:03:08 +0530

    Modified Files

--- WebServer/server.js
+++ WebServer/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 // Middleware configuration
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // Add this to handle URL-encoded data
 
 // Serve static files
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get("/", (req, res) => {
-    res.sendFile("index.html");
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get("/api/hello", (req, res) => {
@@ -21,24 +23,23 @@ app.get("/api/hello", (req, res) => {
 });
 
 app.post("/api/login", (req, res) => {
-    let fileName = 'credentials.json';
+    let fileName = path.join(__dirname, 'credentials.json');
     fs.readFile(fileName, (err, data) => {
         if (err) throw err;
-        let url = req.url;
         let credentials = JSON.parse(data.toString());
         let userData = req.body;
         let userFound = credentials.find((user) => user.email === userData.email && user.password === userData.password);
 
         if (userFound) {
-            res.sendFile("welcome.html");
+            res.sendFile(path.join(__dirname, 'public', 'welcome.html'));
         } else {
-            res.sendFile("login.html");
+            res.sendFile(path.join(__dirname, 'public', 'login.html'));
         }
     });
 });
 
 app.post("/api/register", (req, res) => {
-    let fileName = 'credentials.json';
+    let fileName = path.join(__dirname, 'credentials.json');
     fs.readFile(fileName, (err, data) => {
         if (err) throw err;
 
@@ -48,7 +49,7 @@ app.post("/api/register", (req, res) => {
 
         fs.writeFile(fileName, JSON.stringify(credentials, null, 2), (err) => {
             if (err) throw err;
-            res.sendFile("welcome.html");
+            res.sendFile(path.join(__dirname, 'public', 'welcome.html'));
         });
     });
 });
