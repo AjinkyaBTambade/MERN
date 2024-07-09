8388f6576cf08c69ed5745d225314d33b456ded2
Ajinkya <ajinkyatambade143@gmail.com>
2024-07-09 19:34:22 +0530

    MVC App

---
+++ MVC_App/Models/dal.js
@@ -0,0 +1,11 @@
+exports.getPersonalDetails=()=>{
+    let person={
+        name: "Ajinkya",
+        FullName: "Tambade Ajinkya",
+        age: "19",
+        education: "Bachelor of Engineering in Information Technology",
+        email: "ajinkyatambade143@gmail.com",
+        contact: "7410562485"
+    };
+    return person;
+}
--- MVC_App/controllers/Controller.js
+++ MVC_App/controllers/Controller.js
@@ -1,50 +1,61 @@
+// // ejs module can download using npm install
+// const ejs=require('ejs');
+
+// // trying to send index.ejs file at appropriate http request
+// // which is implementing hollywood rule{don't call me, i will call you}
+// exports.home=(req,res)=>{
+//     ejs.renderFile("./views/index.ejs",{},{},(err,template)=>{
+//        if(err)
+//         {
+//             throw err;
+//         }
+//         else
+//         {
+//             res.end(template);
+//         }
+//     });
+// };
+
+// // http request: /about
+// exports.about = (req, res) => {
+//   ejs.renderFile("./views/about.ejs", {}, {}, (err, template) => {
+//     if (err) {
+//       throw err;
+//     } else {
+//       res.end(template);
+//     }
+//   });
+// };
+
+// // http request: /contact
+// exports.contactus = (req, res) => {
+//   ejs.renderFile("./views/contact.ejs", {}, {}, (err, template) => {
+//     if (err) {
+//       throw err;
+//     } else {
+//       res.end(template);
+//     }
+//   });
+// };
+
+// // http request: /service
+// exports.services = (req, res) => {
+//   ejs.renderFile("./views/service.ejs", {}, {}, (err, template) => {
+//     if (err) {
+//       throw err;
+//     } else {
+//       res.end(template);
+//     }
+//   });
+// };
+
 // ejs module can download using npm install
-const ejs=require('ejs');
+const dal=require('../model/dal');
 
 // trying to send index.ejs file at appropriate http request
 // which is implementing hollywood rule{don't call me, i will call you}
-exports.home=(req,res)=>{
-    ejs.renderFile("./views/index.ejs",{},{},(err,template)=>{
-       if(err)
-        {
-            throw err;
-        }
-        else
-        {
-            res.end(template);
-        }
-    });
-};
 
-// http request: /about
-exports.about = (req, res) => {
-  ejs.renderFile("./views/about.ejs", {}, {}, (err, template) => {
-    if (err) {
-      throw err;
-    } else {
-      res.end(template);
-    }
-  });
-};
-
-// http request: /contact
-exports.contactus = (req, res) => {
-  ejs.renderFile("./views/contact.ejs", {}, {}, (err, template) => {
-    if (err) {
-      throw err;
-    } else {
-      res.end(template);
-    }
-  });
-};
-
-// http request: /service
-exports.services = (req, res) => {
-  ejs.renderFile("./views/service.ejs", {}, {}, (err, template) => {
-    if (err) {
-      throw err;
-    } else {
-      res.end(template);
-    }
-  });
+exports.index = (req, res) => {
+  let model = dal.getPersonalDetails();
+  res.render("../views/index.ejs", model);
 };
