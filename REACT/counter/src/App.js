b3efedbfc73264e6ec5b46a4307155594dfb0aaa
Ajinkya Tambade <ajinkyatambade143@gmail.com>
2024-08-27 09:24:39 +0530

    Counter Functionality

--- REACT/counter/src/App.js
+++ REACT/counter/src/App.js
@@ -1,40 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react'
-
-
+import { useState } from 'react';
 
 function App() {
 
-  const [counter, setCounter] = useState(15)
-
-  //let counter = 15
+  const [counter, setCounter] = useState(15);
 
   const addValue = () => {
-    //counter = counter + 1
-    setCounter(prevCounter => prevCounter + 1)
-
-  }
+    setCounter(prevCounter => {
+      if (prevCounter < 20) {
+        return prevCounter + 1;
+      }
+      return prevCounter;
+    });
+  };
 
   const removeValue = () => {
-    setCounter(counter - 1)
-  }
+    setCounter(prevCounter => {
+      if (prevCounter > 0) {
+        return prevCounter - 1;
+      }
+      return prevCounter;
+    });
+  };
 
   return (
     <>
       <h1>Chai aur react</h1>
       <h2>Counter value: {counter}</h2>
 
-      <button
-        onClick={addValue}
-      >Add value {counter}</button>
+      <button onClick={addValue}>
+        Add value {counter}
+      </button>
       <br />
-      <button
-        onClick={removeValue}
-      >remove value {counter}</button>
+      <button onClick={removeValue}>
+        Remove value {counter}
+      </button>
       <p>footer: {counter}</p>
     </>
-  )
+  );
 }
 
 export default App;
