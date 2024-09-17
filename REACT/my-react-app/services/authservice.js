f0c18b33d70a5b5508334206445f8f13c39b0a9a
Ajinkya Tambade <ajinkyatambade143@gmail.com>
2024-09-17 11:15:34 +0530

    changes

---
+++ REACT/my-react-app/services/authservice.js
@@ -0,0 +1,50 @@
+//data
+var users = [
+    { id: 1, email: 'ravi.tambade@transflower.in', password:'seed', firstname:"Ravi", lastname:"Tambade",contactnumber:"9881735801" },
+    { id: 2, email: 'shubhangi.tambade@transflower.in', password:' transflower', firstname:"Ravi", lastname:"Tambade",contactnumber:"9881735801" },
+    { id: 3, email: 'sanika.bhor@gmail.com', password:'pvg', firstname:"Sankia", lastname:"Bhor",contactnumber:"9881735801" },
+    { id: 4, email: 'nikhil.navale@gmail.com', password:'seed', firstname:"Ravi", lastname:"Tambade",contactnumber:"9881735801" },
+    { id: 5, email: 'shreedhar.patil@gmail.com', password:'seed', firstname:"Ravi", lastname:"Tambade",contactnumber:"9881735801" },
+    { id: 6, email: 'Sharan.kulkarni@gmail.com', password:'seed', firstname:"Ravi", lastname:"Tambade",contactnumber:"9881735801" },
+    { id: 7, email: 'kartik.g@gmail.com', password:'seed', firstname:"Ravi", lastname:"Tambade",contactnumber:"9881735801" },    
+    { id: 8, email: 'seema.patil@gmail.com', password:'seed', firstname:"Ravi", lastname:"Tambade",contactnumber:"9881735801" },
+];
+ 
+//Service
+ 
+const AuthService = {
+ 
+    //service methods
+   
+     validate(email, password){
+        let user= users.find( (theUser)=>(theUser.email ===email && theUser.password===password));
+        if(user ==undefined){
+            return false;
+        }
+        return true;
+     },
+ 
+     getAllUsers() {    
+         return users;
+     },
+ 
+     getUserById(id){
+        let user= users.find( (theUser)=>(theUser.id ===id));
+        return user;
+     },
+ 
+     register(theUser){
+         users.push(theUser);
+     },
+ 
+     Update(theUser){
+         users=users.filter(user=>user.id !==theUser.id);
+         users.push(theUser);
+     },
+ 
+     remove(id){
+         users=users.filter(theUser=>theUser.id !==id);
+     }
+ }
+ 
+ export default AuthService;
---
+++ REACT/my-react-app/src/login.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import AuthService from "../services/authservice";
+ 
+function Login() {
+    //Define state for login
+    const [email, setEmail]=useState('ravi.tambade@transflower.in');
+    const [password, setPassword]=useState('seed');
+ 
+    //change state based on text change event of text box (input)
+    const onChangeEmail=(event)=>{
+      setEmail(event.target.value);   //state is always changed using setter method defined in state
+    }
+ 
+    const onChangePassword=(event)=>{
+      setPassword(event.target.value);
+    }
+ 
+    const onLogin=(event)=>{
+      console.log("on login method is getting invoked");
+        try{
+          let status=false;
+          console.log(email, password);
+          status=AuthService.validate(email, password);
+          if(status ===true){    
+            console.log("login successful");
+          }
+          else{  
+            console.log("login failed");
+          }
+        }
+        catch(error){
+          console.log(error);
+        }
+    }
+ 
+  return (
+    <>
+      <div>
+       
+        <table>
+        <tr>
+          <th>Label</th>
+          <th>Data</th>
+     
+        </tr>
+          <tr>
+            <td>Email</td>
+            <td><input type="email" value={email} onChange={ onChangeEmail}/></td>
+          </tr>
+          <tr>
+            <td>Pasword</td>
+            <td><input type="password" value={password} onChange={ onChangePassword}/></td>
+          </tr>
+          <tr>
+            <td> </td>
+            <td><button  onClick={onLogin}>Login</button></td>
+          </tr>
+        </table>
+       
+        </div>
+    </>
+  )
+}
+ 
+export default Login;
